b046dca9b5bca7b3137905beb39f4ec4f47f50ab
Andrew Reis <areis89@gmail.com>
2018-08-20 10:09:34 -0400

    css complete

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import Axios from 'axios';
 const User = ({ user }) => {
   return (
     <div className="each-user">
-      <h3>{user.name}</h3>
-      <ul>
+      <h3 className="title">{user.name}</h3>
+      <div className="things">
         {user.owns.map(own => {
-          return <li key={own.id}>{own.thing.name}</li>;
+          return <p key={own.id}>{own.thing.name}</p>;
         })}
-      </ul>
+      </div>
     </div>
   );
 };
@@ -30,7 +30,7 @@ const AllUsersFiltered = ({ userList }) => {
 
 const FilterButton = ({ filterThings, filterUsers }) => {
   return (
-    <button className="btn" onClick={() => filterUsers()}>
+    <button onClick={() => filterUsers()}>
       {filterThings ? 'Show All Users' : 'Only Show Users With Things'}
     </button>
   );
@@ -58,17 +58,19 @@ class Main extends Component {
 
   render() {
     let PageView = this.state.filterByThings ? (
-      <AllUsersFiltered className="all-users" userList={this.state.users} />
+      <AllUsersFiltered userList={this.state.users} />
     ) : (
-      <AllUsers className="all-users" userList={this.state.users} />
+      <AllUsers userList={this.state.users} />
     );
     return (
       <div>
-        <FilterButton
-          filterThings={this.state.filterByThings}
-          filterUsers={this.filterUsers}
-        />
-        {PageView}
+        <div className="btn-container">
+          <FilterButton
+            filterThings={this.state.filterByThings}
+            filterUsers={this.filterUsers}
+          />
+        </div>
+        <div className="users-container">{PageView}</div>
       </div>
     );
   }
